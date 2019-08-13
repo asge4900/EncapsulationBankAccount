01af362636ed63630251d9038fa0124b0c9593b7
Asger Bo Lassen <asge4900@edu.campusvejle.dk>
2019-08-13 14:51:38 +0200

    commit

---
+++ Account/Account.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Account
+{
+    public class Account
+    {
+        private int id;
+        private decimal balance;
+        private DateTime created;
+
+        public Account(int id, decimal balance, DateTime created)
+        {
+            Id = id;
+            Balance = balance;
+            Created = created;
+        }
+
+        public int Id {
+                        get {
+                                return id; 
+                            }
+                        set {
+                                
+                            }
+                       }
+
+        public decimal Balance { get { return balance; } set { } }
+
+        public DateTime Created { get { return created; } set { } }
+
+
+        public Account(decimal initalBalance)
+        {
+
+        }
+
+        public void Withdraw(decimal amount)
+        {
+
+        }
+
+        public void Desposit(decimal amount)
+        {
+
+        }
+
+        //public int GetDaysSinceCreation()
+        //{
+
+        //}
+
+        public static (bool isValid, string errorMessage) ValidateId(int id)
+        {
+            return (true, "");
+        }
+
+        public static (bool isValid, string errorMessage) ValidateBalance(decimal balance)
+        {
+            return (true, "");
+        }
+    }
+}
---
+++ AccountTest/AccountTest.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace AccountTest
+{
+    public class AccountTest
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
