4a51a51fb4b32a9bea954eafac3f42d0403511b3
Asger Bo Lassen <asge4900@edu.campusvejle.dk>
2019-08-14 14:58:31 +0200

    made  some changes

--- Account/Account.cs
+++ Account/Account.cs
@@ -1,12 +1,28 @@
 ﻿using System;
 
-namespace Account
+namespace EncapsulationBankAcount.AccountLib
 {
     public class Account
     {
+        #region Fields
         private int id;
         private decimal balance;
         private DateTime created;
+        #endregion
+
+        /// <summary>
+        /// Initializes a new instance of this class. Use for existing records.
+        /// </summary>
+        /// <param name="id">The persistence id genereated by database</param>
+        /// <param name="balance">The balance of the acount.</param>
+        /// <param name="created">The date</param>
+
+        #region Constructors
+
+        public Account()
+        {
+           
+        }
 
         public Account(int id, decimal balance, DateTime created)
         {
@@ -14,21 +30,40 @@ namespace Account
             Balance = balance;
             Created = created;
         }
+        #endregion
 
+        #region Properties
         public int Id {
                         get {
                                 return id; 
                             }
                         set {
-                                
+                                //if (id < 1)
+                                //    throw new ArgumentException("Must be higher than zero");                               
+                                id = value;
                             }
                        }
 
-        public decimal Balance { get { return balance; } set { } }
+        public decimal Balance {
+                                get {
+                                    return balance;
+                                    }
+                                set {
+                                    var validationResult = ValidateBalance(value);
+                                    if (!validationResult.isValid)
+                                    {
+                                    throw new ArgumentOutOfRangeException(validationResult.errorMessage);
+                                    }
+                                    if (balance != value)
+                                    balance = value;
+                                    }
+                                }
 
-        public DateTime Created { get { return created; } set { } }
+        public DateTime Created { get { return created; } set { created = value; } }
+        #endregion
 
 
+        #region Methods
         public Account(decimal initalBalance)
         {
 
@@ -36,27 +71,42 @@ namespace Account
 
         public void Withdraw(decimal amount)
         {
-
+            string message = "";
+            if (amount <= 0)
+                throw new ArgumentException("Hæv et beløb der er større end 0");
+            else if (amount > 25000)
+                message = "Du kan maks hæve 25000";
+            else
+                balance -= amount;
         }
 
         public void Desposit(decimal amount)
         {
-
+            string message = "";
+            if (amount <= 0)
+                message = "Indsæt et beløb der er større end 0";
+            else if (amount > 25000)
+                message = "Indsæt et beløb der er mindre end 25000";
+            else
+                balance += amount;
         }
 
-        //public int GetDaysSinceCreation()
-        //{
-
-        //}
-
-        public static (bool isValid, string errorMessage) ValidateId(int id)
-        {
-            return (true, "");
+        public int GetDaysSinceCreation()
+        {            
+            return (DateTime.Now - created).Days;
         }
+
+
 
         public static (bool isValid, string errorMessage) ValidateBalance(decimal balance)
         {
-            return (true, "");
+            if (balance < -999_999_999)
+                return (false, "Saldoen er for lav");
+            else if (balance > 999_999_999)
+                return (false, "Saldoen er for høj");
+            else            
+                return (true, "");                    
         }
+        #endregion
     }
 }
--- AccountTest/AccountTest.cs
+++ AccountTest/AccountTest.cs
@@ -1,13 +1,74 @@
+using EncapsulationBankAcount.AccountLib;
 using System;
 using Xunit;
 
-namespace AccountTest
+
+namespace EncapsulationBankAcount.AccountTest
 {
     public class AccountTest
     {
         [Fact]
-        public void Test1()
+        public void validate()
         {
+            
+            Account account = new Account(1, 100, DateTime.Today);
+            
+        }
+
+
+        [Fact]
+        public void Validate2()
+        {
+            int expectedDays = 1;
+
+            Account account = new Account();
+            account.Created = DateTime.Now.AddDays(-1);
+
+            int actualDays = account.GetDaysSinceCreation();
+
+            Assert.Equal(expectedDays, actualDays);
+        }
+
+        [Fact]
+        public void Validate3()
+        {
+            decimal expectedBalance = 110;
+
+            Account account = new Account();
+            account.Balance = 100;
+
+            account.Desposit(10);
+
+            decimal actualBalance = account.Balance;
+
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+
+        [Fact]
+        public void Validate4()
+        {
+            decimal expectedBalance = 90;
+
+            Account account = new Account();
+            account.Balance = 100;
+
+            account.Withdraw(10);
+
+            decimal actualBalance = account.Balance;
+
+            Assert.Equal(expectedBalance, actualBalance);
+        }
+
+
+        [Theory]
+        [InlineData(-1000000000)]
+        [InlineData(1000000000)]
+        public void ValidateBalance(decimal invalidbalance)
+        {
+
+            (bool isValid, string errMsg) = Account.ValidateBalance(invalidbalance);
+
+            Assert.False(isValid, "Balance should be valid");
 
         }
     }
